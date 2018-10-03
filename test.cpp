c30ead14851a22d81942e7298c3f462c6c276e77
Sebastian Fortuna <sebastianfortuna5@gmail.com>
2018-10-03 10:57:07 +0930

    addeed test.cpp

---
+++ test.cpp
@@ -0,0 +1,29 @@
+#include "vehicle.h"
+#include "staff.h"
+#include "salesman.h"
+#include "salesyard.h"
+
+#include <iostream>
+#include <string>
+
+int main (void) {
+	numSalesyard = 0;
+	maxSalesyards = 5;
+	salesyard *yards;
+	yards = new salesyard[maxSalesyards];
+
+	yard[numSalesyard]=newSalesyard();
+}
+
+salesyard newSalesyard() {
+	std::cout << "What would you like to name your salesyard?: ";
+	std::cin >>  std::string tempname;
+
+	std::cout << ""
+
+
+
+
+	salesyard tempSalesyard();
+	return tempSalesyard;
+}
