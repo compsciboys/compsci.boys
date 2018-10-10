ca79a225c547962267d83d1312bafe603b2aac53
Sebastian Fortuna <sebastianfortuna5@gmail.com>
2018-10-10 14:52:04 +1030

    as

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 #include <string>
 
 int main (void) {
-	numSalesyard = 0;
-	maxSalesyards = 5;
+	bool run = true;
+	std::string menu = 
+	char userInput;
+	int numSalesyard = 0;
+	int maxSalesyards = 5;
 	salesyard *yards;
 	yards = new salesyard[maxSalesyards];
 
@@ -16,14 +19,36 @@ int main (void) {
 
 	salesyard newSalesyard() {
 	
-	std::cout << "What would you like to name your salesyard?: ";
-	std::cin >>  std::string tempname;
+	
 
-	std::cout << ""
+	while(run==true) {
+		std::cout << "Salesyard Program (press the corresponding key to select an option)\n0 - exit\n 1 - Create new Salesyard\n 2 - Select Salesyard\n" << std::endl;
+		std::cin.get(char) >> userInput;
 
+		switch (userInput) {
+			case '0':
+				run=false;
+				break;
+			case '1':
+				if (numSalesyard<maxSalesyards) {
+					std::cout << "What would you like to name your salesyard?: ";
+					std::cin >>  std::string tempname;
 
+					std::cout << "Wha" 
+				} else {
+					std::cout << "Sorry you have already have " << numSalesyards << " salesyards, the max amount of salesyards allowed" << std::endl;
+				}		
+				break;
+			case '2'
 
+				break;
+			case '~'
 
-	salesyard tempSalesyard();
-	return tempSalesyard;
+				break;
+			default:
+				std::cout << "That is an invalid input" << std::endl;
+
+		}
+	}	
 }
+
